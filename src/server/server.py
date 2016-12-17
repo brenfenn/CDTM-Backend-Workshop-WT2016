0953a8f439799f92de1ac721f56314921e9528c2
Brendan Fennessy <brendanjfennessy@gmail.com>
2016-12-17 15:53:50 +0100

    Save after using whole server from delete function on

--- src/server/server.py
+++ src/server/server.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 # coding: utf8
 
-from flask import Flask, send_file,jsonify, json
+from flask import Flask, send_file, request, jsonify
 import sys
+
+from utils import json_abort
 
 from task import Task
 from list import List
@@ -11,11 +13,16 @@ from list import List
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-myList = List('Inbox', id='0')
+VERSION = 4.0
+
+myLists = [
+    List('Inbox', id='0'),
+    List('Groceries', id='1')
+]
 myTasks = [
-    Task('Think about lunch', '1', id='0', status=Task.COMPLETED),
-    Task('Become a pro in backend development', '0', id='1', status=Task.NORMAL),
-    Task('CONQUER THE WORLD!', '0', id='2', status=Task.NORMAL)
+    Task('Think about lunch', '1', id='0', status = Task.COMPLETED),
+    Task('Become a pro in backend development', '0', id='1', status= Task.NORMAL),
+    Task('CONQUER THE WORLD!', '0', id='2', status = Task.NORMAL)
 ]
 
 # Note: Setting static_url_path to '' has the following effect:
@@ -25,29 +32,29 @@ myTasks = [
 #   - We need this, so that the front-end works properly.
 app = Flask(__name__, static_url_path='')
 
-
-
-VERSION =5.0
-
+# MARK: Static routes
 @app.route('/', methods=['GET'])
 def frontEnd():
     return send_file('static/index.html')
 
-@app.route('/api/version', methods =['GET'])
-def API_JSON_VERSION():
+# MARK: General routes
+@app.route('/api/version', methods=['GET'])
+def get_version():
     return jsonify({'version': VERSION})
 
-@app.route('/api/lists', methods = ['GET'])
-def API_JSON_LIST():
-    dict_list=myList.__dict__
-    return jsonify({'lists': [dict_list]})
+# MARK: List routes
+@app.route('/api/lists', methods=['GET'])
+def get_lists():
+    response = {}
+    response['lists'] = [l.__dict__ for l in myLists]
+    return jsonify(response)
 
-@app.route('/api/lists/<string:list_id>/tasks', methods = ['GET'])
-def API_JSON_task(list_id):
-    dict_task= {}
-    dict_task['tasks'] = [t.__dict__ for t in myTasks if t.list == list_id]
-    return jsonify(dict_task)
-
+# MARK: Task routes
+@app.route('/api/lists/<string:list_id>/tasks', methods=['GET'])
+def get_tasks(list_id):
+    response = {}
+    response['tasks'] = [t.__dict__ for t in myTasks if t.list==list_id]
+    return jsonify(response)
 
 # CREATE ROUTE
 @app.route('/api/lists/<string:list_id>/tasks', methods=['POST'])
@@ -81,28 +88,22 @@ def create_task(list_id):
     # 5. return new task
     return jsonify(newTask.__dict__)
 
-
+# DESTROY ROUTE
 @app.route('/api/lists/<string:list_id>/tasks/<string:task_id>', methods=['DELETE'])
-def delete_task(list_id,task_id):
-    # check if there is a real task to be deleted
-
-
-    if (len([l for l in myLists if l.id == list_id]) < 1)
+def remove_task(list_id, task_id):
+    # 1. Check whether the specified list exists
+    if (len([l for l in myLists if l.id == list_id]) < 1):
         json_abort(404, 'List not found')
 
-    if task_id > len(t for t in myTasks)
-        json_abort(404,"Task not found")
+    # 2. Check whether the specified task exists
+    tasks = [t for t in myTasks if t.id == task_id and t.list == list_id]
+    if (len(tasks) < 1):
+        json_abort(404, 'Task not found')
 
-    tasks = [t for t in myTasks if t.list == list_id and t.id == task_id]
+    # 3. finally remove the task
+    myTasks.remove(tasks[0])
 
-
-
-    dict_task['tasks'] = [t.__dict__ for t in myTasks if t.list == list_id and t.id !=task_id]
-
-    return jsonify(dict_task)
-
-
-
+    return jsonify({'result': True})
 
 if __name__ == '__main__':
-    app.run(host='localhost', port=1337, debug=True)
+    app.run(host='localhost', port=20005, debug=True)
