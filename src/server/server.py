16fa79fc5972a5720409fb8365b3dc53ba4e8a70
Brendan Fennessy <brendanjfennessy@gmail.com>
2016-12-17 14:33:01 +0100

    API Functioning before with GET - version,list,tasks

--- src/server/server.py
+++ src/server/server.py
@@ -13,9 +13,9 @@ sys.setdefaultencoding('utf-8')
 
 myList = List('Inbox', id='0')
 myTasks = [
-    Task('Think about lunch', '0', id='0', status = Task.COMPLETED),
-    Task('Become a pro in backend development', '0', status= Task.NORMAL),
-    Task('CONQUER THE WORLD!', '0', status = Task.NORMAL)
+    Task('Think about lunch', '1', id='0', status=Task.COMPLETED),
+    Task('Become a pro in backend development', '0', id='1', status=Task.NORMAL),
+    Task('CONQUER THE WORLD!', '0', id='2', status=Task.NORMAL)
 ]
 
 # Note: Setting static_url_path to '' has the following effect:
@@ -41,8 +41,15 @@ def API_JSON_VERSION():
 def API_JSON_LIST():
     dict_list=myList.__dict__
     return jsonify({'lists': [dict_list]})
-@app.route('/api/tasks', methods = ['GET'])
-dict_task=
+
+@app.route('/api/lists/<string:list_id>/tasks', methods = ['GET'])
+def API_JSON_task(list_id):
+    dict_task= {}
+    dict_task['tasks'] = [t.__dict__ for t in myTasks if t.list == list_id]
+    return jsonify(dict_task)
+
+
+
 
 if __name__ == '__main__':
     app.run(host='localhost', port=1337, debug=True)
